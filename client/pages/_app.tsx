c45eaf3277e99302a48d9f953a2818c96ce40d1c
Phuoc Tran <phuoctran27@gmail.com>
2021-03-10 22:44:32 -0800

    Set up global theme with MUI

---
+++ client/pages/_app.tsx
@@ -0,0 +1,24 @@
+import { useEffect } from 'react';
+import { AppProps } from 'next/app';
+import { ThemeProvider } from '@material-ui/core/styles';
+
+import '../theme/styles.css';
+import { theme } from '../theme';
+
+const App = ({ Component, pageProps }: AppProps) => {
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement!.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+};
+
+export default App;
---
+++ client/theme/mui-theme.ts
@@ -0,0 +1,38 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+declare module '@material-ui/core/styles/createBreakpoints' {
+  interface BreakpointOverrides {
+    xs: false; // removes the `xs` breakpoint
+    sm: false;
+    md: false;
+    lg: false;
+    xl: false;
+    zero: true;
+    mobile: true;
+    tablet: true;
+    laptop: true;
+    laptopL: true;
+    desktop: true;
+  }
+}
+
+const muiTheme = createMuiTheme({
+  /**
+   * Breakpoints example:
+   *
+   * - theme.breakpoints.up('tablet') will apply to screen sizes >= 769
+   * - theme.breakpoints.down('tablet') will apply to screen sizes < 769
+   */
+  breakpoints: {
+    values: {
+      zero: 0, // xs ranges from 0 to 425, inclusive
+      mobile: 426,
+      tablet: 769,
+      laptop: 1025,
+      laptopL: 1441,
+      desktop: 1921,
+    },
+  },
+});
+
+export default muiTheme;
