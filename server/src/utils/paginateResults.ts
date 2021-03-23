9d197aab6d30d764ae216ac14e3eb04aaba0ec1e
Phuoc Tran <phuoctran27@gmail.com>
2021-03-23 15:08:57 -0700

    Refactor paginate function
    The old function assumes that the cursor is the same as the cursor index
    in the results array. This assumption is false when the results array is
    filtered before paginating.

--- server/src/utils/paginateResults.ts
+++ server/src/utils/paginateResults.ts
@@ -4,30 +4,34 @@ interface PaginateResultsInput {
   cursor?: number;
   pageSize?: number;
   results: any[];
+  getCursor?: any;
 }
 
 const paginateResults = ({
   cursor,
   pageSize = DEFAULT_PAGE_SIZE,
   results,
+  getCursor = () => null,
 }: PaginateResultsInput) => {
-  if (pageSize < 1) {
-    return [];
-  }
+  if (pageSize < 1) return [];
 
-  if (typeof cursor === 'undefined') {
-    return results.slice(0, Math.min(pageSize, results.length));
-  }
+  if (!cursor) return results.slice(0, pageSize);
+  const cursorIndex = results.findIndex((item) => {
+    // if an item has a `cursor` on it, use that, otherwise try to generate one
+    let itemCursor = item.cursor ? item.cursor : getCursor(item);
 
-  // note: cursors are 1-indexed
-  if (cursor <= 0) {
-    return results.slice(0, Math.min(pageSize, results.length));
-  } else if (cursor >= results.length) {
-    return [];
-  }
+    // if there's still not a cursor, return false by default
+    return itemCursor ? cursor === itemCursor : false;
+  });
 
-  // case: 0 < cursor < results.length
-  return results.slice(cursor, Math.min(cursor + pageSize, results.length));
+  return cursorIndex >= 0
+    ? cursorIndex === results.length - 1 // don't let us overflow
+      ? []
+      : results.slice(
+          cursorIndex + 1,
+          Math.min(results.length, cursorIndex + 1 + pageSize)
+        )
+    : results.slice(0, pageSize);
 };
 
 export default paginateResults;
